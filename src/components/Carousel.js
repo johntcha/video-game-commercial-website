00461478668d39aa1c6ca31f051efb4babffd968
John Tcha <johntcha94@gmail.com>
2020-10-06 11:09:29 +0200

    carousel component fixed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./Carousel.css";
 
-export default function CarouselComponent() {
+export default function CarouselComponent(props) {
     return (
         <div class="carousel-wrapper">
             <Carousel infiniteLoop autoPlay 
@@ -14,27 +14,24 @@ export default function CarouselComponent() {
             transitionTime={700}
             >
                 <div>
-                    <img src="/images/towertest.png" />
-                    <h2>Destruction Tower pawaaaa</h2>
+                    <img src={props.img1} />
+                    <h2>{props.first_h2}</h2>
                     <p>
-                    Destructive tours have the most powerful damage since its attacks
-        have an area effect but is very slow.
+                    {props.first_p}
                     </p>
                 </div>
                 <div>
-                    <img src="/images/towertest2.jpg" />
-                    <h2>Slow Tower pawaaaa</h2>
+                    <img src={props.img2} />
+                    <h2>{props.second_h2}</h2>
                     <p>
-                    Slowing towers are the most adapted if the enemy is strong
-        or is fast. At strategic places, this tower can totally make the game.
+                    {props.second_p}
                     </p>
                 </div>
                 <div>
-                    <img src="/images/towertest3.jpeg" />
-                    <h2>Fast Tower pawaaaa</h2>
+                    <img src={props.img3} />
+                    <h2>{props.third_h2}</h2>
                     <p>
-                    Fast attacks towers is essential for fast and weak enemy as they can kill them
-        quicly. However, they only posess single targeted enemy and week attacks.
+                    {props.third_p}
                     </p>
                 </div>
             </Carousel>
--- src/components/Game.js
+++ src/components/Game.js
@@ -9,7 +9,20 @@ function Game(){
 			<h2>TOWERS</h2>
 			<div className="game_wrapper">
 				<Tabs />
-				<CarouselComponent />
+				<CarouselComponent 
+				first_h2="Destruction Tower pawaaaa"
+				second_h2="Slow Tower pawaaaa"
+				third_h2="Fast Tower pawaaaa"
+				first_p="Destructive tours have the most powerful damage since its attacks
+        have an area effect but is very slow."
+				second_p="Slowing towers are the most adapted if the enemy is strong
+        or is fast. At strategic places, this tower can totally make the game."
+				third_p="Fast attacks towers is essential for fast and weak enemy as they can kill them
+        quicly. However, they only posess single targeted enemy and week attacks."
+        		img1="/images/towertest.png"
+        		img2="/images/towertest2.jpg"
+        		img3="/images/towertest3.jpeg"
+				 />
 			</div>
 			<h2>UNITIES</h2>
 			<div className="unity_wrapper">
